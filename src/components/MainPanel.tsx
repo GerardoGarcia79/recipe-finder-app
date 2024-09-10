c380bcf25b88256111311a4d0041d7c0d19be056
Gerardo de Leon <gerardo-leon10@hotmail.com>
2024-09-10 14:30:50 -0600

    Styling MainPanel component

--- src/components/MainPanel.tsx
+++ src/components/MainPanel.tsx
@@ -1,5 +1,25 @@
+import { IoSearch } from "react-icons/io5";
+
 const MainPanel = () => {
-  return <div className="bg-[#f8f8fa] flex flex-grow">Main section</div>;
+  return (
+    <div className="bg-[#f8f8fa] flex flex-grow justify-center">
+      <div className="w-full max-w-[1100px] flex flex-col bg-cyan-400">
+        <form className="w-full">
+          <div className="relative">
+            <IoSearch className="absolute top-[23px] left-2" size="18px" />
+            <input
+              type="text"
+              placeholder="What do you want to cook today?"
+              className="w-full shadow-lg rounded-sm text-base mt-4 mb-3 p-1 ps-8"
+            />
+          </div>
+        </form>
+        <h1 className="font-bold text-3xl">Recommended Recipes</h1>
+        <p className="text-sm">Popular choices</p>
+        <div>Recipes</div>
+      </div>
+    </div>
+  );
 };
 
 export default MainPanel;
