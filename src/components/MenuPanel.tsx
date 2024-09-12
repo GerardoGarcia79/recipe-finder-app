007145084fc80014ac2d58cfbe402d722b49df65
Gerardo de Leon <gerardo-leon10@hotmail.com>
2024-09-12 09:33:34 -0600

    Implement navLinks and Links to MenuPanel component

--- src/components/MenuPanel.tsx
+++ src/components/MenuPanel.tsx
@@ -1,22 +1,47 @@
 import recipes from "../assets/recipes.png";
 import { FaHome } from "react-icons/fa";
 import { AiOutlineHeart } from "react-icons/ai";
+import { Link, NavLink } from "react-router-dom";
 
 const MenuPanel = () => {
   return (
     <div className="md:w-1/6 bg-white p-4 flex flex-col ">
-      <div className="flex justify-center">
-        <img src={recipes} className="w-48" />
-      </div>
+      <Link to="/">
+        <div className="flex justify-center">
+          <img src={recipes} className="w-48" />
+        </div>
+      </Link>
       <ul className="flex flex-col mt-3 md:mt-16">
-        <li className="flex items-center justify-center mb-3">
-          <FaHome size="24px" />
-          <p className="text-xl ml-1">Home</p>
-        </li>
-        <li className="flex items-center justify-center">
-          <AiOutlineHeart size="24px" />
-          <p className="text-xl ml-1">Favorites</p>
-        </li>
+        <NavLink
+          to="/"
+          className={({ isActive, isPending }) =>
+            isPending
+              ? "pending"
+              : isActive
+              ? "active underline underline-offset-2"
+              : ""
+          }
+        >
+          <li className="flex items-center justify-center mb-3">
+            <FaHome size="24px" />
+            <p className="text-xl ml-1">Home</p>
+          </li>
+        </NavLink>
+        <NavLink
+          to="/favorites"
+          className={({ isActive, isPending }) =>
+            isPending
+              ? "pending"
+              : isActive
+              ? "active underline underline-offset-2"
+              : ""
+          }
+        >
+          <li className="flex items-center justify-center">
+            <AiOutlineHeart size="24px" />
+            <p className="text-xl ml-1">Favorites</p>
+          </li>
+        </NavLink>
       </ul>
     </div>
   );
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import App from "./App";
+import FavoritesPage from "./pages/FavoritesPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [{ index: true, element: <App /> }],
+    children: [
+      { index: true, element: <App /> },
+      { path: "/favorites", element: <FavoritesPage /> },
+    ],
   },
 ]);
 
